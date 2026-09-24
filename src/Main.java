6a7328470387a924369dc688366b376826696982
mouaouda <mohamed.ouaouda@lyceeastier.com>
2026-09-24 11:38:12 +0200

    Improved switch case logic and added 08X recognition

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,15 @@ public class Main {
 
         String zoneGeo = nbTel.substring(0, 2);
         String zoneGeoDep = nbTel.substring(2, 4);
+        String zoneIndicType = nbTel.substring(0, 2);
+        String zoneIndicOp = nbTel.substring(2, 4);
+        String nbVertGratuit = nbTel.substring(2, 4);
+        String nbGratuit = nbTel.substring(2, 4);
+        String nbPayantOne = nbTel.substring(2, 3);
+        String nbPayantTwo = nbTel.substring(2, 3);
+        String nbPayantNine = nbTel.substring(2, 3);
+        System.out.println(nbPayantOne);
+        String nbIndicatif = nbTel.substring(0,2);
 
         switch (zoneGeo) {
             case "01":
@@ -121,6 +130,77 @@ public class Main {
                         System.out.println("Charente");
                         break;
                 }
+                break;
+
+            case "06":
+                switch (zoneIndicOp) {
+                    case "08":
+                        System.out.println("06 08 Orange");
+                        break;
+                    case "09":
+                        System.out.println("06 09 SFR");
+                        break;
+                    case "10":
+                        System.out.println("06 10 SFR");
+                        break;
+                    case "01":
+                        System.out.println("06 11 Neuf Cegetel");
+                        break;
+                }
+                System.out.println("Numéro de téléphon 06 non documenté dans la base");
+                break;
+            case "07":
+                System.out.println("Numéro de téléphone 07 classique");
+                break;
+
+            case "08":
+                switch (nbGratuit) {
+                    case "00":
+                        System.out.println("08 numéro vert gratuit");
+                        break;
+                    case "02":
+                        System.out.println("08 numéro vert gratuit");
+                        break;
+                    case "03":
+                        System.out.println("08 numéro vert gratuit");
+                        break;
+                    case "04":
+                        System.out.println("08 numéro vert gratuit");
+                        break;
+                    case "05":
+                        System.out.println("08 numéro vert gratuit");
+                        break;
+                    case "06":
+                        System.out.println("08 numéro gratuit");
+                        break;
+                    case "07":
+                        System.out.println("08 numéro gratuit");
+                        break;
+                    case "08":
+                        System.out.println("08 numéro gratuit");
+                        break;
+                    case "09":
+                        System.out.println("08 numéro gratuit");
+                        break;
+                }
+                switch (nbPayantOne) {
+                    case "1":
+                        System.out.println("081X numéro payant ( vous paierez au maximum 6 centimes d’euros la\n" + "minute, ou 15 centimes d’euros l’appel. )");
+                        break;
+                }
+                switch (nbPayantTwo) {
+                    case "2":
+                        System.out.println("082X numéro payant ( vous paierez au maximum 20 centimes d’euro par\\n\" + \"minute, ou 50 centimes d’euros par appel. )");
+                        break;
+                }
+                switch (nbPayantNine) {
+                    case "9":
+                        System.out.println("089X numéro payant ( vous paierez au maximum 80 centimes d’euros\n" + "par minute ou 3 euros par appel. )");
+                        break;
+                }
+                break;
+            case "09":
+                System.out.println("09 VoIP");
                 break;
 
             default:
